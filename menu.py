9ae893cea32ff8e88f188a5eda5213d054421e18
manorajesh <mano@raje.sh>
2022-07-19 19:11:38 +0530

    Major changes

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import curses
 import typing
+from textwrap import fill
 
 '''
 title
@@ -8,6 +9,8 @@ Timed Test - [120s] [60s] [30s] [15s] [Custom]
 
 Wordlist - [Common] [Wacky] [Source_Code]
 '''
+
+longest_len = len("Timed Test - [120s] [60s] [30s] [15s] [Custom]")//2
 
 class colors:
     OKBLUE = '\033[94m'
@@ -20,25 +23,75 @@ class colors:
     RESET = '\033[0m'
 
 def init():
+    gray = 500
+    global longest_len
     scr = curses.initscr()
+    lines, columns = scr.getmaxyx()
     curses.curs_set(0)
     curses.noecho()
     curses.cbreak()
 
-    lines, columns = scr.getmaxyx()
-    scr.addstr(lines//2-4, columns//4, "title", curses.A_REVERSE)
-    scr.addstr(lines//2-2, columns//4, "Timed Test - [120s] [60s] [30s] [15s] [Custom]")
-    scr.addstr(lines//2, columns//4, "Wordlist - [Common] [Wacky] [Source_Code]")
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_color(1, gray, gray, gray) # gray
+    curses.init_color(2, 603, 819, 831) # blue
+    curses.init_color(3, 969, 773, 624) # peach
+    curses.init_color(4, 929, 145, 306) # red
+    curses.init_color(5, 63, 588, 282) # green
+
+    curses.init_pair(1, 1, -1) # gray
+    curses.init_pair(2, 2, -1) # blue
+    curses.init_pair(3, 3, -1) # peach
+    curses.init_pair(4, 4, -1) # red
+    curses.init_pair(5, 5, -1) # green
+
+    if lines % 2 != 0: # if terminal is not even, add a line
+        offset = 1
+    else:
+        offset = 0
+
+    scr.addstr(lines//2-4+offset, columns//2-longest_len, "title", curses.A_REVERSE | curses.color_pair(2))
+    scr.addstr(lines//2-2+offset, columns//2-longest_len, "Timed Test - [120s] [60s] [30s] [15s] [Custom]", curses.color_pair(3))
+    scr.addstr(lines//2+offset, columns//2-longest_len, "Wordlist - [Common] [Wacky] [Source_Code]", curses.color_pair(5))
+    for i, val in enumerate(fill("Use the arrow keys to select an option; press space to select and TAB to exit", columns//2).split("\n")):
+            scr.addstr(lines//2+3+offset+i, columns//2-longest_len, val, curses.color_pair(1))
     return scr
 
 def printToTerminal(message, scr):
+    global longest_len
     lines, columns = scr.getmaxyx()
     message = message.split("\n\r")
 
     print("\n" * (lines//2-4) + "\r")
     for i in range(3):
-        print(" " * (columns//4) + message[i] + "\r\n")
+        print(" " * (columns//2-longest_len) + message[i] + "\r\n")
     print("\n" * (lines//2-4) + "\r")
+
+def curses_input(scr, prompt=""):
+    lines, columns = scr.getmaxyx()
+    if lines % 2 != 0: # if terminal is not even, add a line
+        offset = 1
+    else:
+        offset = 0
+
+    scr.addstr(lines//2+3+offset, columns//2-longest_len, prompt, curses.A_BOLD)
+    curses.echo()
+    curses.curs_set(1)
+    number = []
+    while True:
+        c = scr.getch()
+        if 47 < c < 58:
+            number.append(int(chr(c)))
+        elif c == 10:
+            break
+        elif c == 127:
+            number.pop()
+
+    curses.curs_set(1)
+    curses.noecho()
+    if len(number) == 0:
+        return 42 # indeed, 42 is the answer to the ultimate question of life, the universe, and everything
+    return "".join(str(i) for i in number)
 
 menuscr = init()
 menuscr.keypad(True)
@@ -52,13 +105,17 @@ while (ch := menuscr.getch()) != 9:
         pointer += 1
     elif ch == curses.KEY_LEFT:
         pointer -= 1
-    elif (ch == 32 or curses.KEY_ENTER) and 0 < pointer < 5: # range of times
-        typing.main(int(selectable_text[pointer][0:2]), pointer-6)
-    elif (ch == 32 or curses.KEY_ENTER) and pointer == 5: # custom time
-        typing.main(input("Enter time: "))
-    elif (ch == 32 or curses.KEY_ENTER) and 5 < pointer < 9: # wordlist
+    elif (ch == 32 or 10) and 0 < pointer < 5: # range of times
+        typing_inst = typing.TypingTest(int(selectable_text[pointer][0:2]), int(selectable_text.index(wordlist))-6)
+    elif (ch == 32 or 10) and pointer == 5: # custom time
+        typing_inst = typing.TypingTest(int(curses_input(menuscr, "Enter time in seconds: ")), int(selectable_text.index(wordlist))-6)
+    elif (ch == 32 or 10) and 5 < pointer < 9: # wordlist
         wordlist = selectable_text[pointer]
 
     pointer = pointer%len(selectable_text)
     active = selectable_text[pointer]
+    menuscr.clear()
+    menuscr.refresh()
     printToTerminal(whole_text.replace(active, colors.REVERSED + active + colors.RESET).replace(wordlist, colors.OKGREEN + wordlist + colors.RESET), menuscr)
+
+curses.endwin()
--- typing.py
+++ typing.py
@@ -1,9 +1,9 @@
-import os
 from textlists import wordlists
 import curses
 from numpy.random import choice
 from time import sleep
 from threading import Thread
+from textwrap import fill
 
 # Print word list to terminal
 # Check for user input
@@ -20,128 +20,166 @@ class colors:
     REVERSED = '\033[7m'
     RESET = '\033[0m'
 
-def wrap(text, width):
-    global immut_words
-    new_text = ""
-    message = []
-    width_increment = width
-    for i, letter in enumerate(text):
-        if min(width, immut_words[i:].index(" ")+i) == width:
-            message.append(new_text)
-            new_text = ""
-            width += width_increment
-        new_text += letter
-    return message
+class TypingTest:
+    def __init__(self, time, wordlist_index):
+        self.stdscr = curses.initscr()
+        self.immut_words = choice(wordlists[wordlist_index], size=60)
+        self.immut_words = self.splitLetters(self.immut_words)
+        self.words = list(self.immut_words)
+        self.max_length = len("".join(self.wrap(self.immut_words, self.stdscr.getmaxyx()[1]//3)[0:3]))
+        self.timer = time
+        self.flag = True
+        self.longest_sentence = len(max(self.wrap(self.immut_words, self.stdscr.getmaxyx()[1]//3)[0:3], key=len))//2
+        self.initDisplay()
+        self.main(time, wordlist_index)
 
-def printToTerminal(message):
-    global timer
-    columns, lines = os.get_terminal_size()
-    message = wrap(message, columns//3)
+    def wrap(self, text, width):
+        new_text = ""
+        message = []
+        width_increment = width
+        for i, letter in enumerate(text):
+            if min(width, self.immut_words[i:].index(" ")+i) == width:
+                message.append(new_text)
+                new_text = ""
+                width += width_increment
+            new_text += letter
+        return message
 
-    print("\n" * (lines//2-4) + "\r")
-    print(" " * (columns//3) + colors.OKBLUE + str(timer) + colors.RESET + "\n" + "\r")
-    for i in range(3):
-        print(" " * (columns//3) + message[i] + "\r")
-    print("\n" * (lines//2-4) + "\r")
+    def initDisplay(self):
+        gray = 500
+        curses.update_lines_cols()
+        lines, columns = self.stdscr.getmaxyx()
+        message = self.wrap(self.words, columns//3)
+        self.stdscr.clear()
+        curses.curs_set(0)
 
-def splitLetters(list):
-    new_list = []
-    for word in list:
-        for letter in word:
-            new_list.append(letter)
-        new_list.append(" ")
-    return new_list
+        # colors
+        curses.start_color()
+        curses.use_default_colors()
+        curses.init_color(1, gray, gray, gray) # gray
+        curses.init_color(2, 603, 819, 831) # blue
+        curses.init_color(3, 969, 773, 624) # peach
+        curses.init_color(4, 929, 145, 306) # red
+        curses.init_color(5, 63, 588, 282) # green
+        curses.init_color(6, gray+150, gray+150, gray+150) # light gray
 
-def timer_func():
-    global timer
-    global words
-    global flag
-    while timer > 0 and flag:
-        printToTerminal(words)
-        sleep(1)
-        timer -= 1
+        curses.init_pair(1, 1, -1) # gray
+        curses.init_pair(2, 2, -1) # blue
+        curses.init_pair(3, 3, -1) # peach
+        curses.init_pair(4, 4, -1) # red
+        curses.init_pair(5, 5, -1) # green
+        curses.init_pair(6, 6, -1) # light gray
 
-immut_words = 0
-words = 0
-max_length = 0
-timer = 60 # seconds
-flag = True
-
-def main(time, wordlist_index):
-    global immut_words
-    global max_length
-    global words
-    global flag
-    global timer
-
-    immut_words = choice(wordlists[wordlist_index], size=60)
-    immut_words = splitLetters(immut_words)
-    words = list(immut_words)
-    max_length = len("".join(wrap(immut_words, os.get_terminal_size()[0]//3)[0:3]))
-
-    counter = 0
-    usrInput = ""
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    curses.curs_set(0)
-    stdscr.keypad(True)
-    stdscr.clear()
-    correct_typed = 0
-    incorrect_typed = 0
-
-    timer_proc = Thread(target=timer_func)
-    timer_proc.start()
-
-    words[counter] = colors.REVERSED + words[counter] + colors.RESET
-    while usrInput != 9 and timer > 0: # 9 is the key for exit (TAB)
-        printToTerminal(words)
-        usrInput = stdscr.getch()
-
-        if chr(usrInput) == immut_words[counter]:
-            words[counter] = colors.OKGREEN + words[counter][4] + colors.RESET
-            correct_typed += 1
-        elif usrInput == 127: # 127 is the key for backspace
-            words[counter] = immut_words[counter]
-            words[counter-1] = immut_words[counter-1]
-            counter -= 2
-        elif immut_words[counter] == " ":
-            words[counter] = colors.FAIL + "_" + colors.RESET
-            incorrect_typed += 1
+        if lines % 2 != 0: # if terminal is not even, add a line
+            offset = 1
         else:
-            words[counter] = colors.FAIL + words[counter][4] + colors.RESET
-            incorrect_typed += 1
-        counter += 1
-        if counter >= max_length:
-            immut_words = choice(wordlists[wordlist_index], size=60)
-            immut_words = splitLetters(immut_words)
-            words = list(immut_words)
-            counter = 0
-            max_length = len("".join(wrap(immut_words, os.get_terminal_size()[0]//3)[0:3]))
+            offset = 0
 
-        words[counter] = colors.REVERSED + words[counter] + colors.RESET
-    
-    flag = False
-    timer_proc.join()
+        self.stdscr.addstr(lines//2-3+offset, columns//2-self.longest_sentence, str(self.timer), curses.color_pair(1))
+        self.stdscr.addstr(lines//2-1+offset, columns//2-1-self.longest_sentence, " ", curses.A_REVERSE)
+        for i in range(3):
+            self.stdscr.addstr(lines//2-1+i+offset, columns//2-self.longest_sentence, message[i], curses.color_pair(1))
+            self.stdscr.refresh()
+        for i, val in enumerate(fill("Press any key to start; press TAB to exit", columns//3).split("\n")):
+            self.stdscr.addstr(lines//2+3+offset+i, columns//2-self.longest_sentence, val, curses.color_pair(6))
 
-    curses.nocbreak()
-    stdscr.keypad(False)
-    curses.echo()
-    curses.endwin()
-    
-    try:
-        total_entries = correct_typed + incorrect_typed
-        total_time = (time - timer)/60
-        print("correct: %s, incorrect: %s, time: %s, timer: %s, time-timer: %s, total time: %s, total entries: %s\n\r" % (correct_typed, incorrect_typed, time, timer, time-timer, total_time, total_entries))
+        self.stdscr.getch()
 
-        net_wpm = round(abs(total_entries/5 - incorrect_typed)/total_time)
-        accuracy = round(correct_typed / total_entries * 100)
-        raw_wpm = round(total_entries/5/total_time)
-    except ZeroDivisionError:
-        net_wpm = 0
-        accuracy = 0
-        raw_wpm = 0
+    def printToTerminal(self, message):
+        curses.update_lines_cols()
+        lines, columns = self.stdscr.getmaxyx()
+        message = self.wrap(message, columns//3)
 
-    print(f"Your net WPM: {net_wpm}\n\rYour accuracy: {accuracy}%\n\rYour raw WPM: {raw_wpm}")
+        print("\n" * (lines//2-4) + "\r")
+        print(" " * (columns//2-self.longest_sentence) + colors.OKBLUE + str(self.timer) + colors.RESET + "\n" + "\r")
+        for i in range(3):
+            print(" " * (columns//2-self.longest_sentence) + message[i] + "\r")
+        print("\n" * (lines//2-4) + "\r")
 
-main(60, 1)
+    def splitLetters(self, list):
+        new_list = []
+        for word in list:
+            for letter in word:
+                new_list.append(letter)
+            new_list.append(" ")
+        return new_list
+
+    def timer_func(self):
+        while self.timer > 0 and self.flag:
+            self.printToTerminal(self.words)
+            sleep(1)
+            self.timer -= 1
+
+    def main(self, time, wordlist_index):
+        counter = 0
+        usrInput = ""
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(0)
+        self.stdscr.keypad(True)
+        correct_typed = 0
+        incorrect_typed = -1 # registers first click as incorrect which is unintended
+
+        timer_proc = Thread(target=self.timer_func)
+        timer_proc.start()
+
+        self.words[counter] = colors.REVERSED + self.words[counter] + colors.RESET
+        while usrInput != 9 and self.timer > 0: # 9 is the key for exit (TAB)
+            self.stdscr.refresh()
+            self.printToTerminal(self.words)
+            usrInput = self.stdscr.getch()
+
+            if chr(usrInput) == self.immut_words[counter]:
+                self.words[counter] = colors.OKGREEN + self.words[counter][4] + colors.RESET
+                correct_typed += 1
+            elif usrInput == 127: # 127 is the key for backspace
+                self.words[counter] = self.immut_words[counter]
+                self.words[counter-1] = self.immut_words[counter-1]
+                counter -= 2
+            elif self.immut_words[counter] == " ":
+                self.words[counter] = colors.FAIL + "_" + colors.RESET
+                incorrect_typed += 1
+            else:
+                self.words[counter] = colors.FAIL + self.words[counter][4] + colors.RESET
+                incorrect_typed += 1
+            counter += 1
+            if counter >= self.max_length:
+                self.immut_words = choice(wordlists[wordlist_index], size=60)
+                self.immut_words = self.splitLetters(self.immut_words)
+                self.words = list(self.immut_words)
+                counter = 0
+                self.max_length = len("".join(self.wrap(self.immut_words, self.stdscr.getmaxyx()[1]//3)[0:3]))
+
+            self.words[counter] = colors.REVERSED + self.words[counter] + colors.RESET
+        
+        self.flag = False
+        timer_proc.join()
+        
+        try:
+            total_entries = correct_typed + incorrect_typed
+            total_time = (time - self.timer)/60
+
+            net_wpm = round(abs(total_entries/5 - incorrect_typed)/total_time)
+            accuracy = round(correct_typed / total_entries * 100)
+            raw_wpm = round(total_entries/5/total_time)
+        except ZeroDivisionError:
+            net_wpm = 0
+            accuracy = 0
+            raw_wpm = 0
+
+        lines, columns = self.stdscr.getmaxyx()
+        
+        self.stdscr.clear()
+        self.stdscr.addstr(lines//2-4, columns//3, "Results:", curses.A_UNDERLINE)
+        self.stdscr.addstr(lines//2-2, columns//3, "WPM: " + str(net_wpm), curses.color_pair(2))
+        self.stdscr.addstr(lines//2-1, columns//3, "Accuracy: " + str(accuracy) + "%", curses.color_pair(3))
+        self.stdscr.addstr(lines//2, columns//3, "Correct: " + str(correct_typed), curses.color_pair(5))
+        self.stdscr.addstr(lines//2+1, columns//3, "Incorrect: " + str(incorrect_typed), curses.color_pair(4))
+        self.stdscr.addstr(lines//2+2, columns//3, "Raw WPM: " + str(raw_wpm), curses.color_pair(1))
+        self.stdscr.addstr(lines//2+4, columns//3, "Press any key to exit", curses.color_pair(6))
+        self.stdscr.getch()
+
+        curses.endwin()
+        
+if __name__ == "__main__":
+    test = TypingTest(time=60, wordlist_index=0) # 0 = Common
